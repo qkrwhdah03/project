f9f0328f98892071b2b1b71be6be3df1d7239d2d
qkrwhdah03 <qkrwhdah03@naver.com>
2025-11-23 07:49:54 +0000

    viser cubemap added

--- viewer/utils.py
+++ viewer/utils.py
@@ -1,6 +1,7 @@
 import time 
 import numpy as np
 from PIL import Image
+from scipy.spatial.transform import Rotation as R
 
 def read_image(path: str)-> np.array:
     img = Image.open(path).convert("RGB")
@@ -10,3 +11,20 @@ def wait():
     while True:
         time.sleep(1.0)
     return 
+
+def get_face_quaternion(
+    target_view: np.array, # unit vector
+    target_up: np.array, # unit vector
+    base_view: np.array = np.array([0.0, 0.0, 1.0]),
+    base_up: np.array = np.array([0.0, -1.0, 0.0])
+)-> np.array:
+    base_left = np.cross(base_up, base_view)
+    target_left = np.cross(target_up, target_view)
+
+    base = np.stack([base_up, base_view, base_left], axis=1)
+    target = np.stack([target_up, target_view, target_left], axis=1)
+
+    rot_mat = target @ base.T
+
+    quat = R.from_matrix(rot_mat).as_quat()  # [x, y, z, w]
+    return np.array([quat[3], quat[0], quat[1], quat[2]])  # wxyz
--- viewer/viewer.py
+++ viewer/viewer.py
@@ -1,20 +1,105 @@
+from typing import List
+from utils import wait, read_image, get_face_quaternion
+
+import os
+import numpy as np
 import viser
-from utils import wait
 
-class Viewer:
-    def __init__(self):
+
+class CubeMap:
+    def __init__(
+        self,
+        name: str,
+        dir_path: str, 
+        prefix: str,
+        suffixes: List[str] = ["posx", "posy", "posz", "negx", "negy", "negz"],
+        ext: str = ".png"
+    )-> None:
+        self.name = name
+        
+        paths = [os.path.join(dir_path, prefix + "_" + suffix + ext) for suffix in suffixes]
+        self.images = {}
+        for suffix, path in zip(suffixes, paths):
+            self.images[suffix] = read_image(path) # list of [W, H, C] shaped uint8 numpy ndarray
+        
+        return
+    
+    def get(self, key: str)-> np.ndarray:
+        return self.images[key]
+
+class CubeMapViewer:
+    def __init__(
+        self,
+        cube_render_x: int = 100,
+        cube_render_y: int = 100,
+        cube_render_z: int = 100,
+        keys: List[str] = ["posx", "posy", "posz", "negx", "negy", "negz"]
+    )-> None:
+        assert len(keys) == 6
         self.server = viser.ViserServer(host="0.0.0.0", port=8080)
-        self.server.scene.set_environment_map(background=True)
+        self.cube_render_x = cube_render_x
+        self.cube_render_y = cube_render_y
+        self.cube_render_z = cube_render_z
+        self.keys = keys
 
-        self.server.scene.add_box(
-            name = "box"
-        )
+        # Note that the scene is reverted upside down.
+        self.pos = {
+            "posx": np.array([self.cube_render_x/2, 0, 0]),
+            "posy": np.array([0, self.cube_render_y/2, 0]),
+            "posz": -np.array([0, 0, self.cube_render_z/2]),
+            "negx": np.array([-self.cube_render_x/2, 0, 0]),
+            "negy": np.array([0, -self.cube_render_y/2, 0]),
+            "negz": -np.array([0, 0, -self.cube_render_z/2]),
+        }
 
-    def run(self):
+        self.wxyz = {
+            "posx": get_face_quaternion(-np.array([1., 0., 0.]), np.array([0., -1., 0.])),
+            "posy": get_face_quaternion(-np.array([0., 1., 0.]), -np.array([0., 0., 1.])),
+            "posz": -get_face_quaternion(np.array([0., 0., 1.]), np.array([0., -1., 0.])),
+            "negx": get_face_quaternion(-np.array([-1., 0., 0.]), np.array([0., -1., 0.])),
+            "negy": get_face_quaternion(-np.array([0., -1., 0.]), -np.array([0., 0., -1.])),
+            "negz": -get_face_quaternion(np.array([0., 0., -1.]), np.array([0., -1., 0.])),
+        }
+        
+
+        self.width = {
+            "posx": self.cube_render_z,
+            "posy": self.cube_render_x,
+            "posz": self.cube_render_x,
+            "negx": self.cube_render_z,
+            "negy": self.cube_render_x,
+            "negz": self.cube_render_x
+        }
+        self.height = {
+            "posx": self.cube_render_y,
+            "posy": self.cube_render_z,
+            "posz": self.cube_render_y,
+            "negx": self.cube_render_y,
+            "negy": self.cube_render_z,
+            "negz": self.cube_render_y
+        }
+        return
+    
+    def add_cubemap(self, cubemap: CubeMap)-> None:
+        for key in self.keys:
+            viewer.server.scene.add_image(
+                name = f"{cubemap.name}_{key}",
+                image = cubemap.get(key),
+                render_width = self.width[key],
+                render_height = self.height[key],
+                wxyz = self.wxyz[key],
+                position = self.pos[key],
+                visible = True
+            )
+        return
+
+    def run(self)-> None:
         print("Viewer running at http://localhost:8080")
         wait()
 
 
 if __name__ == "__main__":
-    viewer = Viewer()
+    viewer = CubeMapViewer(cube_render_x= 100, cube_render_y= 150, cube_render_z= 100)
+    cubemap = CubeMap(name="cubemap", dir_path="/root/project/data/cubemap", prefix="0")
+    viewer.add_cubemap(cubemap)
     viewer.run()
